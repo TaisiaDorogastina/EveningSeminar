44d25c5464392b673abcb1b33a4852640b7d6f63
TaisiaDorogastina <124715549+TaisiaDorogastina@users.noreply.github.com>
2023-03-06 20:34:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿Задача 21
+Напишите программу, которая принимает на вход координаты двух точек
+и находит расстояние между ними в 3D пространстве
+
+double Distance3D(double x1, double y1, double x2, double y2, double z1, double z2)
+{
+double res = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+return Math.Round(res, 2);
+}
+
+Console.Write("Введите координату по Х для первой точки ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату по У для первой точки ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату по Х для второй точки ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату по Y для второй точки ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату по Z для третьей точки ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату по Z для третьей точки ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double distance = Distance3D(x1, y1, x2, y2, z1, z2);
+System.Console.WriteLine($"Расстояние между точками = {distance}");
+
+Задача 23
+
+Напишите программу, которая принимает на вход число (N)
+и выдаёт таблицу кубов чисел от 1 до N
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i <= number; i++)
+{
+Console.Write($"{Math.Pow(i, 3)} ");
+}
+
+Задача 19
+
+Напишите программу, которая принимает на вход пятизначное число и проверяет,
+является ли оно палиндромом.
+
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Ваше число: {number} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введи правильное число");
+
